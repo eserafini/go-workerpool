3198c3717bd0c2924fa6eaa58d8066b037f36482
Eduardo Serafini <eduardo.serafini@57501.local>
2025-06-14 18:01:09 -0300

    feat: main comments

--- main.go
+++ main.go
@@ -3,29 +3,31 @@ package main
 import (
 	"context"
 	"fmt"
-	"math/rand"
 	"sync"
 	"time"
 
 	"github.com/eserafini/go-workerpool/pkg/workerpool"
 )
 
+// Define your job type
 type NumberJob struct {
 	Number int
 }
 
+// Define your result type
 type NumberResult struct {
 	Value     int
 	WorkerID  int
 	Timestamp time.Time
 }
 
-func proccessNumber(ctx context.Context, job workerpool.Job) workerpool.Result {
+// Define your processing function
+func processNumber(ctx context.Context, job workerpool.Job) workerpool.Result {
 	number := job.(NumberJob).Number
 	workerID := number % 3
 
-	sleepTime := time.Duration(800+rand.Intn(400)) * time.Millisecond
-	time.Sleep(sleepTime)
+	// Simulate some work
+	time.Sleep(100 * time.Millisecond)
 
 	return NumberResult{
 		Value:     number,
@@ -35,40 +37,43 @@ func proccessNumber(ctx context.Context, job workerpool.Job) workerpool.Result {
 }
 
 func main() {
-	maxValue := 20
-	bufferSize := 10
-
-	pool := workerpool.New(proccessNumber, workerpool.Config{
+	// Create a new worker pool with 3 workers
+	pool := workerpool.New(processNumber, workerpool.Config{
 		WorkerCount: 3,
 	})
 
-	inputCh := make(chan workerpool.Job, bufferSize)
+	// Create input channel with buffer size
+	inputCh := make(chan workerpool.Job, 10)
 	ctx := context.Background()
 
+	// Start the worker pool
 	resultCh, err := pool.Start(ctx, inputCh)
 	if err != nil {
 		panic(err)
 	}
 
+	// Create a wait group to wait for all results
 	var wg sync.WaitGroup
-	wg.Add(maxValue)
+	wg.Add(20) // Number of jobs to process
 
-	fmt.Println("Initializing pool with max ", maxValue)
-
+	// Send jobs to the pool
 	go func() {
-		for i := 0; i < maxValue; i++ {
+		for i := 0; i < 20; i++ {
 			inputCh <- NumberJob{Number: i}
 		}
 		close(inputCh)
 	}()
 
+	// Process results
 	go func() {
 		for result := range resultCh {
 			r := result.(NumberResult)
-			fmt.Printf("Number: %d", r.Value)
+			fmt.Printf("Processed number %d by worker %d at %v\n",
+				r.Value, r.WorkerID, r.Timestamp)
 			wg.Done()
 		}
 	}()
 
+	// Wait for all results
 	wg.Wait()
 }
